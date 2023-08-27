4c822557d16549912d6436af8b338022889b2fba
cagrikutayok <cagrikutayok@hotmail.com>
2023-08-27 15:56:31 +0300

    pub

--- src/kafka/kafka-consumer.ts
+++ src/kafka/kafka-consumer.ts
@@ -3,21 +3,23 @@ import { Kafka, Consumer, EachMessagePayload } from 'kafkajs';
 export class KafkaConsumer {
   private consumer: Consumer;
 
-  constructor(brokers: string[], groupId: string) {
+  constructor(topic: string, groupId: string, brokers: string[]) {
     const kafka = new Kafka({ brokers });
     this.consumer = kafka.consumer({ groupId });
 
-    this.consumer.run({
-      eachMessage: async ({ topic, partition, message }: EachMessagePayload) => {
-        // Handle incoming message
-        const payload = JSON.parse(message.value?.toString()!);
-        console.log(`Received message on topic ${topic}:`, payload);
-      },
-    });
+    this.runConsumer(topic);
   }
 
-  async subscribe(topic: string) {
-    await this.consumer.subscribe({ topic });
+  async runConsumer(topic: string) {
+    await this.consumer.connect();
+    await this.consumer.subscribe({ topic, fromBeginning: true });
+
+    await this.consumer.run({
+      eachMessage: async (payload: EachMessagePayload) => {
+        const messageContent = payload.message.value?.toString();
+        console.log(`Received message: ${messageContent}`);
+      },
+    });
   }
 
   async disconnect() {
