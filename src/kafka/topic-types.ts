807660f53262adabd59201c77cf4da876b25c2fc
cagrikutayok <cagrikutayok@hotmail.com>
2023-08-29 21:44:24 +0300

    pub

--- src/kafka/topic-types.ts
+++ src/kafka/topic-types.ts
@@ -2,3 +2,4 @@ export const MOTOR_CREATED = 'motor-created';
 export const MOTOR_UPDATED = 'motor-updated';
 export const MOTOR_DELETED = 'motor-deleted';
 export const USER_CREATED = 'user-created';
+export const LICENSE_CREATED = 'license-created';
